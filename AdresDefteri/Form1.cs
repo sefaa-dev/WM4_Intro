91c64e5a7549594c16ea888bd12a72ef94d6e71a
sefaa-dev <sefayilmaz582@gmail.com>
2021-10-27 13:05:15 +0300

    Add project files.

---
+++ AdresDefteri/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using AdresDefteri.Models;
+
+namespace AdresDefteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Kisi kisi = new Kisi();
+        Ders ders = new Ders();
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            try
+            {
+                kisi.Ad = "Kamil";
+                kisi.Soyad = "³Fıdıl";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
+            } // set
+            MessageBox.Show(kisi.Soyad); //get
+        }
+    }
+}
---
+++ AdresDefteri/Kisi.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace AdresDefteri
+{
+    class Kisi
+    {
+        //Access Modifiers - erişim belirleyici
+        /*
+         * public
+         * private
+         * internal
+         * *protected
+         * *internal protected 
+         */
+
+        //Fields
+
+        private string _ad;
+        private string _soyad;
+
+        //encapsulation
+
+        public string Ad
+        {
+            set // value 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new FormatException("Kişinin adı boş geçilemez");
+                }
+
+                foreach (char harf in value)
+                {
+                    if (char.IsDigit(harf))
+                        throw new Exception("Kişinin adı rakam bulunumaz");
+                    if (char.IsSymbol(harf) || char.IsControl(harf) || char.IsPunctuation(harf))
+                        throw new Exception("Kişinin adı geçersiz karakter bulunmaktadır.");
+                }
+
+                if (value.Length > 50)
+                    throw new Exception("Girilen ifade 50 karakterden fazla olamaz");
+                _ad = value;
+            }
+            get //return
+            {
+                return _ad.Substring(0, 1).ToUpper() + _ad.Substring(1).ToLower();
+            }
+        }
+
+        public string Soyad
+        {
+            get
+            {
+                return _soyad.Substring(0, 1).ToUpper() + _soyad.Substring(1).ToLower(); ;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new FormatException("Kişinin soyadı boş geçilemez");
+                }
+
+                foreach (char harf in value)
+                {
+                    if (char.IsDigit(harf))
+                        throw new Exception("Kişinin soyadında rakam bulunumaz");
+                    if (char.IsSymbol(harf) || char.IsControl(harf) || char.IsPunctuation(harf))
+                        throw new Exception("Kişinin soyadında geçersiz karakter bulunmaktadır.");
+                }
+
+                if (value.Length > 50)
+                    throw new Exception("Girilen ifade 50 karakterden fazla olamaz");
+
+                _soyad = value;
+            }
+        }
+
+    }
+    interface IKisi { }
+
+    enum KisiTipleri
+    {
+
+    }
+}

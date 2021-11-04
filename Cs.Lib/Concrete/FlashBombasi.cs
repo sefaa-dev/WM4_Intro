06066955fb37bfa5138c4367445924c560e6f09c
Sefa Yılmaz <sefayilmaz582@gmail.com>
2021-11-04 06:05:11 +0300

    Add files via upload

---
+++ Cs.Lib/Concrete/FlashBombasi.cs
@@ -0,0 +1,28 @@
+﻿using Cs.Lib.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs.Lib.Concrete
+{
+    public sealed class FlashBombasi : Firlatilan
+    {
+        public FlashBombasi()
+        {
+            this.Fiyat = 200;
+            this.Hasar = 0;
+            this.SilahResim = new MemoryStream(Properties.Resources.Flash);
+            this.Ulke = "Almanya";
+        }
+
+
+
+        public override int Firlat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

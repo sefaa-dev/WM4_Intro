c02ac09c24adb08490a828daaeeb623e0dd9f5bf
sefaa-dev <sefayilmaz582@gmail.com>
2021-11-08 09:36:24 +0300

    .

---
+++ MyCoin/Models/BinanceReqExchangeBody.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoin.Models
+{
+    public class BinanceReqExchangeBody
+    {
+        public string Timezone { get; set; }
+        public long ServerTime { get; set; }
+        public Ratelimit[] RateLimits { get; set; }
+        public object[] ExchangeFilters { get; set; }
+        public List<Symbol> Symbols { get; set; }
+
+        public class Rootobject
+        {
+            
+        }
+
+      
+
+       
+
+       
+
+
+    }
+}
---
+++ MyCoin/Models/RateLimit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoin.Models
+{
+    public class Ratelimit
+    {
+        public string RateLimitType { get; set; }
+        public string Interval { get; set; }
+        public int IntervalNum { get; set; }
+        public int Limit { get; set; }
+    }
+}

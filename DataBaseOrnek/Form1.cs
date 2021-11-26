0d0d280c03a5504073a5e54ba682e7b13e855d8a
sefaa-dev <sefayilmaz582@gmail.com>
2021-11-26 14:52:51 +0300

    datab

--- DataBaseOrnek/Form1.cs
+++ DataBaseOrnek/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataBaseOrnek.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,5 +22,7 @@ namespace DataBaseOrnek
         {
 
         }
+
+        NorthwindContext nortwindcontext = new NorthwindContext();
     }
 }

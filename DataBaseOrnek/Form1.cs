20d186de2ebd7e94960f62b9971b38b5a6265be3
sefaa-dev <sefayilmaz582@gmail.com>
2021-11-29 10:35:36 +0300

    .

--- DataBaseOrnek/Form1.cs
+++ DataBaseOrnek/Form1.cs
@@ -20,7 +20,7 @@ namespace DataBaseOrnek
 
         private void Form1_Load(object sender, EventArgs e)
         {
-                
+                 
         }
 
         NorthwindContext nortwindcontext = new NorthwindContext();
